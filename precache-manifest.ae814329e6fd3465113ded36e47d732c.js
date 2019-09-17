60485ac54920a430ea66bd4f07d0414c7f8b0f43
Ritesh Sharma <RSharma@jhacorp.com>
2019-09-17 16:00:14 -0700

    Updates

---
+++ precache-manifest.ae814329e6fd3465113ded36e47d732c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39eba6fe24daf70dd8ddc02efb8ac3e4",
+    "url": "/react-gitpages/index.html"
+  },
+  {
+    "revision": "32530d95569037289d68",
+    "url": "/react-gitpages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c290e6b4140c0eac94c2",
+    "url": "/react-gitpages/static/js/2.de60ffef.chunk.js"
+  },
+  {
+    "revision": "32530d95569037289d68",
+    "url": "/react-gitpages/static/js/main.e30f237e.chunk.js"
+  },
+  {
+    "revision": "2d5415bafb00a2ca57f5",
+    "url": "/react-gitpages/static/js/runtime~main.1097b7e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gitpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
